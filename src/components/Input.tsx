965317552821c387e89d8473121df0ebce2f7b4b
Baptiste Marcon <baptistemcn@icloud.com>
2024-01-07 00:19:18 +0100

    style(input): update input design

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,10 @@
 import { type ComponentPropsWithRef } from "react";
 
 export function Input(props: ComponentPropsWithRef<"input">) {
-  return <input className={"rounded border border-gray-800"} {...props} />;
+  return (
+    <input
+      className={"rounded border border-gray-800 px-4 py-2 dark:text-gray-800"}
+      {...props}
+    />
+  );
 }
