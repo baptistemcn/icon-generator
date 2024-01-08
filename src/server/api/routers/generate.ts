a94bb77cdcf09189a4bd6d53e13482c555fdefef
Baptiste Marcon <baptistemcn@icloud.com>
2024-01-08 18:47:49 +0100

    feat: add credits and remove to the user when sumbit a prompt

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,16 +1,39 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const generateRouter = createTRPCRouter({
-  generatIcon: publicProcedure
+  generatIcon: protectedProcedure
     .input(
       z.object({
         prompt: z.string(),
       }),
     )
-    .mutation(({ input }) => {
+    .mutation(async ({ ctx, input }) => {
       console.log("we are here", input);
+
+      const { count } = await ctx.db.user.updateMany({
+        where: {
+          id: ctx.session.user.id,
+          credits: {
+            gte: 1,
+          },
+        },
+        data: {
+          credits: {
+            decrement: 1,
+          },
+        },
+      });
+
+      if (count <= 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "You do not have enough credits",
+        });
+      }
+
       return {
         message: "success",
       };
